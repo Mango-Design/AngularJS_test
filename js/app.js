828f45c24e584ae8c940902dbcbcbade969827ed
Mango Design <ivan.sollima@gmail.com>
2015-10-31 12:01:52 +0100

    Bower.io

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('main', []);
+(function () {
+
+    var app = angular.module('main', []);
+
+
+    app.controller("GenController", function(){
+
+        this.rpost = data;
+
+    });
+
+    var data = {
+        message: "The quick brown fox jumps over the lazy dog",
+        rating: 1,
+        author: "Mario Rossi",
+        created_at: "2014-10-22T19:59Z"
+    }
+
+})();
